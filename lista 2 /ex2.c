78b8472045d1f91a6226a4fea11ae8647ef08608
vitor-fantin <vfantin5@gmail.com>
2025-03-21 14:25:17 -0300

    Create ex2.c

---
+++ lista 2 /ex2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+  float combustivelGastos, kmPercorridos, gastoDoCombustivel = 5.24;
+  float consumoMedio, totalGasto;
+  
+  printf("Digite quantos litros de combustivel foram gastos: ");
+  scanf("%f", &combustivelGastos);
+  
+  printf("Digite o total de quilometros percorridos: ");
+  scanf("%f", &kmPercorridos);
+  
+  totalGasto = combustivelGastos * gastoDoCombustivel;
+  consumoMedio = kmPercorridos / combustivelGastos;
+  
+  printf("Quantidade de litros gastos: %.2f", combustivelGastos);
+  printf("Total de quilometros percorridos: %.2f", kmPercorridos);
+  printf("Total gasto de combustivel: %.2f", totalGasto);
+  printf("Qual foi o consumo medio de combustivel: %.2f", consumoMedio);
+  
+  return 0;
+  
+}
