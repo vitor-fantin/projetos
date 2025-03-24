beaeb74aca0e5cb0db352b8fd5e762398c402f5c
vitor-fantin <vfantin5@gmail.com>
2025-03-24 20:22:55 -0300

    ex5.c

--- lista 2 /ex5.c
+++ lista 2 /ex5.c
@@ -1,3 +1,10 @@
+/*
+Autor....: vitor fantin
+data.....: 21/03/2025
+titulo...: Calcular comissao de venda de bicicleta
+
+*/
+
 #include <stdio.h>
 int main(){
   int numEmpregados, numBicicletasVend;
